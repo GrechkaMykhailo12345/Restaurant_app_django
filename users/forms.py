4a74b8f5d4f21aa8ae11c7feac082017bbd64ad1
GrechkaMykhailo12345 <yalta010709@gmail.com>
2025-10-07 20:42:10 +0300

    Виправив логін, зробив так щоб можна виходити з аккаунту, і також зробив маленькі зміни в дизайні

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,22 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 class UserRegisterForm(UserCreationForm):
-    
     class Meta:
         model = User
         fields = ('username', 'email')
         
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Додаємо класи Bootstrap
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
+
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Додаємо класи Bootstrap до полів
         for field in self.fields.values():
             field.widget.attrs.update({'class': 'form-control'})
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .forms import UserLoginForm
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='users/login.html',
+        authentication_form=UserLoginForm 
+    ), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
