ce887bf5e3f334502168df2a7e804fbc4a542012
GrechkaMykhailo12345 <yalta010709@gmail.com>
2025-11-08 18:00:56 +0200

    Додав нижню шапку з соціальними мережами і з надписом хто зробив цей сайт

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,6 +123,10 @@ LOGOUT_REDIRECT_URL = 'home_page'
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'restaurant_app', 'static'), 
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
