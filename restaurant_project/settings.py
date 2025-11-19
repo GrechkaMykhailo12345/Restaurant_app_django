50de85141cb1f70e7f212e80713298ad1f4ec52d
GrechkaMykhailo12345 <yalta010709@gmail.com>
2025-11-19 20:24:38 +0200

    Змінив зміни в settings.py

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -122,7 +122,7 @@ LOGOUT_REDIRECT_URL = 'home_page'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
